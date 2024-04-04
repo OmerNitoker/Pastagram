4df1aa67a47469e385500a788acc4e75ac909723
John DoeSamuel <samuelsebban008@gmail.com>
2024-04-04 15:48:46 +0300

    e

--- src/cmps/Search.jsx
+++ src/cmps/Search.jsx
@@ -1,29 +1,13 @@
 import { useEffect, useRef } from 'react';
 
 export function Search({ toggleSidebar }) {
-    const searchRef = useRef(null);
-
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (searchRef.current && !searchRef.current.contains(event.target)) {
-                toggleSidebar();
-            }
-        };
-
-        // Ajoute l'écouteur d'événements
-        document.addEventListener('mousedown', handleClickOutside);
-
-        // Supprime l'écouteur d'événements lors du démontage du composant
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [toggleSidebar]);
+   
 
     return (
-        <div className="search-page" ref={searchRef}>
+        <div className="search-page" >
             <h1>SEARCH</h1>
             <input type="search" />
-            <button className="close-btn" onClick={toggleSidebar}>Close</button>
+           
         </div>
     )
 }
