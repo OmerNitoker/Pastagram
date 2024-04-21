4e50f07ba4da1136a555e55a3d7dec010da53b7c
Omer <omernitoker@gmail.com>
2024-04-21 06:46:19 +0300

    likes and comments hover on posts

--- src/cmps/UserPosts.jsx
+++ src/cmps/UserPosts.jsx
@@ -7,6 +7,7 @@ export function UserPosts({ user, posts }) {
     const [userPosts, setUserPosts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [gridRows, setGridRows] = useState(10); // Initial value pour le nombre de lignes
+    const [isHovered, setIsHovered] = useState(false);
 
     const location = useLocation();
 
@@ -23,7 +24,7 @@ export function UserPosts({ user, posts }) {
             }).filter(Boolean);
 
             setUserPosts(postsToShow)
-            
+
             // Calculer le nombre de lignes nécessaires
             const rowCount = Math.ceil(user.posts.length / 3);
             setGridRows(rowCount);
@@ -57,7 +58,7 @@ export function UserPosts({ user, posts }) {
     //         // Mettre à jour la variable CSS
     //         document.documentElement.style.setProperty('--grid-rows', rowCount);
 
-            
+
     //     } else {
     //         setUserPosts([]); // Réinitialiser les posts si aucun post n'est disponible
     //         setIsLoading(false);
@@ -84,8 +85,14 @@ export function UserPosts({ user, posts }) {
             {userPosts.length > 0 ? (
                 userPosts.map(post => (
                     <Link key={post._id} to={`/post/${post._id}`} state={{ previousLocation: location }}>
-                        <div className="gallery-item">
+                        <div className="gallery-item" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
                             <img src={post.imgUrl} alt="post" className="gallery-image" />
+                            {isHovered && (
+                                <div className="img-overlay">
+                                    <span>Likes: {post.likedBy.length}</span>
+                                    <span>Comments: {post.comments.length}</span>
+                                </div>
+                            )}
                         </div>
                     </Link>
                 ))
